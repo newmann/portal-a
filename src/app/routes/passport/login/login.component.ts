c401cd95abedd95f8f01bec5343f1712240bfcd6
newmann <newmannhu@qq.com>
2018-12-04 17:12:43 +0800

    update

--- src/app/routes/passport/login/login.component.ts
+++ src/app/routes/passport/login/login.component.ts
@@ -132,6 +132,12 @@ export class UserLoginComponent implements OnDestroy {
     // 重新获取 StartupService 内容，我们始终认为应用信息一般都会受当前用户授权范围而影响
     this.startupSrv.load().then(() => this.router.navigate(['/']));
 
+
+  }
+
+  gotoHome(){
+
+    this.router.navigate(["/"]);
   }
 
   // #region social
